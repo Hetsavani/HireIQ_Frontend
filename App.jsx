17583669f0677133bd584ef23c88b20e0b661000
HitenMalvi04 <malvihiten8842@gmail.com>
2025-07-17 16:25:58 +0530

    Initial commit: quiz platform structure with component folders

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './assets/static/css/main.css'
+import './assets/static/css/other1.css'
+import './assets/static/css/other2.css'
+import SideBar from './SideBar'
+import Body from './Body'
+
+function App() {
+
+  return (
+    <>
+      {/* <SideBar></SideBar> */}
+      <Body></Body>
+    </>
+  )
+}
+
+export default App
---
+++ Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Body() {
+    return (
+        <>
+            <h1>Quiz Catagory</h1>
+        </>
+    )
+}
+
+export default Body
