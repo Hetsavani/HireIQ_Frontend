4aace914470a8db2e82a02b377b9bad2874e1e7d
HitenMalvi04 <malvihiten8842@gmail.com>
2025-07-17 19:06:44 +0530

    Sidebar and navbar added

--- src/layouts/CandidateLayout.jsx
+++ src/layouts/CandidateLayout.jsx
@@ -1,1 +1,18 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/common/Sidebar';
+import Navbar from '../components/common/Navbar';
+import './CandidateLayout.css';
 
+const CandidateLayout = () => {
+  return (
+    <div className="app-layout">
+      <Sidebar />
+      <div className="main-section">
+        <Navbar />
+        <Outlet /> 
+      </div>
+    </div>
+  );
+};
+
+export default CandidateLayout;
