1aee2b58b499b64f99d73956d616eac4944953f8
Ronakthesiya <ronakthesiya2004@gmail.com>
2025-07-18 18:22:56 +0530

    FinalDashboard

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Outlet, Route, Routes } from 'react-router-dom';
 import AdminSidebar from '../components/admin/AdminSidebar';
 import AdminNavbar from '../pages/admin/AdminNavbar/AdminNavbar';
-// import './AdminLayout.css'
+import './AdminLayout.css'
 
 const AdminLayout = () => {
   return (
@@ -11,7 +11,10 @@ const AdminLayout = () => {
       <AdminSidebar />
       <div className="w-100">
         <AdminNavbar />
-        <div className="main-content" style={{ marginLeft: '250px' }} >
+        <div className="flex-grow-1 p-4"
+          style={{// Adjust if navbar height is different
+            overflowY: "auto",
+            backgroundColor: "#0f121f", marginLeft: '250px' }} >
           <Outlet/>
         </div>
       </div>
